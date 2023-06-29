4a10d7e310448b51ecb1cde660ea61e2b0d397d0
Odd Karsten Svartaas <oksvart@gmail.com>
2023-06-29 12:09:50 +0200

    Add test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
@@ -10,4 +12,8 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
