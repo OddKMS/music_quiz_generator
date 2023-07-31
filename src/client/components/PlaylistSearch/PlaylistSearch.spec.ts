1c4ad7c2ed1249f21e96cd952136991a2a064e5b
Odd Karsten Svartaas <oksvart@gmail.com>
2023-07-31 16:33:31 +0200

    Scaffold up PlaylistSearch component

---
+++ src/client/components/PlaylistSearch/PlaylistSearch.spec.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import { describe, it } from 'vitest';
+import { PlaylistSearch } from '#components/PlaylistSearch';
+
+describe('the PlaylistSearch component', () => {
+  it.todo('should have an input field', () => {});
+
+  it.todo('should have a button that performs the search', () => {});
+
+  it.todo('should display any errors that result from the search', () => {});
+
+  it.todo('should let the user start the search with the enter key', () => {});
+});
---
+++ src/client/components/PlaylistSearch/index.ts
@@ -0,0 +1,1 @@
+export { PlaylistSearch } from './PlaylistSearch';
