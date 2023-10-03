f1191f85566b0d45eee812cfde6ba4ba7cd9721a
Odd Karsten Svartaas <oksvart@gmail.com>
2023-10-03 08:54:49 +0200

    Create basic PlaylistSearch component

--- src/client/components/PlaylistSearch/PlaylistSearch.tsx
+++ src/client/components/PlaylistSearch/PlaylistSearch.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { PageContext } from '#musicquizgenerator/types';
 
-const PlaylistSearch = () => {};
+const PlaylistSearch = (pageContext: PageContext) => {
+  pageContext.spotifySdk;
+
+  function onSearch() {}
+
+  const component = (
+    <div>
+      <input data-testid="playlistSearch.inputfield" />
+      <button data-testid="playlistSearch.searchbutton" onClick={onSearch} />
+    </div>
+  );
+
+  return component;
+};
 
 export { PlaylistSearch };
